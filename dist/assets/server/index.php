e3927931b0a8d09f6a092a049546394847b0d7cd
Sagar Babar <sagar.babar@globeflexresearch.com>
2018-12-26 16:06:38 +0530

    added php mailer

--- dist/assets/server/index.php
+++ dist/assets/server/index.php
@@ -1,20 +1,27 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-require_once('class.phpmailer.php');
+require_once('PHPMailer.php');
+require_once('Exception.php');
+require_once('OAuth.php');
+require_once('POP3.php');
+require_once('SMTP.php');
 $errors = array();  	// array to hold validation errors
 $data = array(); 		// array to pass back data
+	$request = file_get_contents("php://input");
+	$postdata = json_decode($request);
 // validate the variables ======================================================
-	if (empty($_POST['name']))
+	if (empty($postdata->name))
 		$errors['name'] = 'Name is required.';
-	if (empty($_POST['email']))
+	if (empty($postdata->email))
 		$errors['email'] = 'E-mail is required.';
-	if (empty($_POST['message']))
+	if (empty($postdata->message))
 		$errors['message'] = 'Message is required.';
 // return a response ===========================================================
 	// response if there are errors
 	if ( ! empty($errors)) {
 		// if there are items in our errors array, return those errors
+		$data['data'] = $postdata;
 		$data['success'] = false;
 		$data['errors']  = $errors;
 		
@@ -22,26 +29,22 @@ $data = array(); 		// array to pass back data
 		$mail = new PHPMailer(); // create a new object
 		$mail->IsSMTP(); // enable SMTP
 		$mail->SMTPAuth = true; // authentication enabled
-		$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for GMail
+		// $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for GMail
 		$mail->Host = "localhost";
 		$mail->Port = 25; // or 587
 		$mail->IsHTML(true);
 		$mail->Username = "fpnukaek"; //Email that you setup
         $mail->Password = "ne9w):93DTXI7t"; // Password
-        $mail->From = $_POST['email'];
-        $mail->FromName = $_POST['name'];
-		$mail->Subject = "Y-Web Contact mail from " . $_POST['name'] . ", e-mail: " .$_POST['email']. "";
-		$mail->Body = $_POST['message'];
+        $mail->From = $postdata->email;
+        $mail->FromName = $postdata->name;
+		$mail->Subject = "Y-Web Contact mail from " . $postdata->name . ", e-mail: " .$postdata->email. "";
+		$mail->Body = $postdata->message;
 		$mail->AddAddress("training@caddventures.com", 'CADD VENTURES'); //Pass the e-mail that you setup
-		 if(!$mail->Send())
-		    {
-		    		echo "Mailer Error: " . $mail->ErrorInfo;
-		    }
-		    else
-		    {
-		    	$data['success'] = true;
-	    		$data['message'] = 'Thank you for sending e-mail.';
-		    }
-		
+		 if(!$mail->Send()) {
+				echo "Mailer Error: " . $mail->ErrorInfo;
+		} else {
+			$data['success'] = true;
+			$data['message'] = 'Thank you for sending e-mail.';
+		}
 	}
 	echo json_encode($data);
